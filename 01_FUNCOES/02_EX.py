21e5321e9971c3aa12452f47849ef6aaf9ae8f5d
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-06-06 13:28:58 -0300

    Update 02_EX.py

--- 01_FUNCOES/02_EX.py
+++ 01_FUNCOES/02_EX.py
@@ -21,3 +21,17 @@ def imprimir_mes(numero):
 
 n = int(input("Digite um número de 1 a 12: "))
 imprimir_mes(n)
+
+"""
+PONTOS A MELHORAR:
+
+1. Chamar a funbção main(). 
+    Dica: Usar o 04_EX.py como exemplo
+    
+2. Eu não ensinei esse negócio de 1: "Janeiro", ..., 12: "Fevereiro"
+    Duas opções:
+
+    1) Quero que você escreva um .md explicando o que é e como funciona com as fontes que você usou para fazer isso...
+
+    2) Fazer do jeito que aprendemos em sala.
+"""
