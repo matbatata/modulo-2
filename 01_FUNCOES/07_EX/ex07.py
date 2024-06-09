49ebb519eae769ae2dca4b887b8f61fa3d9fe235
Matheus <142700606+matbatata@users.noreply.github.com>
2024-06-09 16:22:21 -0300

    Exercício 07
    Mais um para coleção

---
+++ 01_FUNCOES/07_EX/ex07.py
@@ -0,0 +1,10 @@
+def converter_para_segundos(horas, minutos, segundos):
+    total_segundos = (horas * 3600) + (minutos * 60) + segundos
+    return total_segundos
+def main():
+    horas = int(input("Digite as horas: "))
+    minutos = int(input("Digite os minutos: "))
+    segundos = int(input("Digite os segundos: "))
+    total_segundos = converter_para_segundos(horas, minutos, segundos)
+    print("O total de segundos é:", total_segundos)
+main()
