391bc4484f9a3658157cd266db924a5dde24b2cf
Matheus <142700606+matbatata@users.noreply.github.com>
2024-06-09 15:58:02 -0300

    estou rushando no domingo e toma esses dois ai
    Fiquei com preguiça de fazer, mas consegui entender e fazer mais esses dois fica de olha que vem mais alguns

---
+++ 01_FUNCOES/03_EX/ex03.py
@@ -0,0 +1,7 @@
+def fahrenheit_para_celsius(temp_fahrenheit):
+    temp_celsius = (temp_fahrenheit - 32.0) * (5.0/9.0)
+    return temp_celsius
+
+temperatura_fahrenheit = float(input("Insira a temperatura em graus Fahrenheit: "))
+temperatura_celsius = fahrenheit_para_celsius(temperatura_fahrenheit)
+print("A temperatura em Celsius é:", temperatura_celsius)
---
+++ 01_FUNCOES/05_EX/ex05.py
@@ -0,0 +1,8 @@
+def calcular_volume_esfera(raio):
+    pi = 3.1414592
+    volume = (4/3) * pi * (raio ** 3)
+    return volume
+
+raio = float(input("Insira o raio da esfera (em metros): "))
+volume = calcular_volume_esfera(raio)
+print("O volume da esfera é:", volume)
