671f830d064c1a633f298d586ea599ac4144e3ac
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-06-10 11:50:47 -0300

    Update ex07.py

--- 01_FUNCOES/07_EX/ex07.py
+++ 01_FUNCOES/07_EX/ex07.py
@@ -1,6 +1,7 @@
 def converter_para_segundos(horas, minutos, segundos):
     total_segundos = (horas * 3600) + (minutos * 60) + segundos
     return total_segundos
+    
 def main():
     horas = int(input("Digite as horas: "))
     minutos = int(input("Digite os minutos: "))
@@ -8,3 +9,5 @@ def main():
     total_segundos = converter_para_segundos(horas, minutos, segundos)
     print("O total de segundos é:", total_segundos)
 main()
+
+# muito bom
