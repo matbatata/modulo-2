ad9277b84b9b5c3fed007e68d0b442b9b66ddf04
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-06-10 11:51:53 -0300

    Update ex08.py

--- 01_FUNCOES/08_EX/ex08.py
+++ 01_FUNCOES/08_EX/ex08.py
@@ -1,10 +1,14 @@
 def eh_quadrado_perfeito(numero):
-    return int(numero ** 0.5) ** 2 == numero
+    return int(numero ** 0.5) ** 2 == numero #ok
+    
 def main():
-    exemplos = [1, 4, 9, 10, 16, 25]
-    for num in exemplos:
+    exemplos = [1, 4, 9, 10, 16, 25] # não aprendemos isso
+    for num in exemplos: # não aprendemos
         if eh_quadrado_perfeito(num):
-            print(num, "é um quadrado perfeito.")
+            print(num, "é um quadrado perfeito.")#ok
         else:
-            print(num, "não é um quadrado perfeito.")
+            print(num, "não é um quadrado perfeito.") #ok
 main() 
+
+
+# crie um .md explicando esse código
