1f6ea89c9c072c70a4096b08b6541e8f6e328d2a
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-06-10 11:46:12 -0300

    Update ex01.py

--- 01_FUNCOES/01_EX/ex01.py
+++ 01_FUNCOES/01_EX/ex01.py
@@ -1,11 +1,16 @@
 def encontrar_maior(n1, n2):
-    if n1 > n2:
+    if n1 >= n2: # alterei aqui
         return n1
     else:
         return n2
+# A lógica estáa correta, mas tem um pequeno problema... Se n1=n2, não vai retornar nada
+
+#muito bom
 def main():
     num1 = float(input("Insira o primeiro número: "))
     num2 = float(input("Insira o segundo número: "))
     maior_numero = encontrar_maior(num1, num2)
     print("O maior número é:", maior_numero)
 main()
+
+# corrigido
