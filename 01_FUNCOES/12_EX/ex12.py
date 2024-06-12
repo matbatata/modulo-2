32205974b2cf45c3d7b6a95214b6526d501c434c
Matheus <142700606+matbatata@users.noreply.github.com>
2024-06-12 18:39:13 -0300

    Exercício 12
    O exercício pede um número positivo e retorne o maior fator primo desse número.

---
+++ 01_FUNCOES/12_EX/ex12.py
@@ -0,0 +1,13 @@
+def maior_fator_primo(numero):
+    fator = 2
+    while fator * fator <= numero:
+        if numero % fator == 0:
+            numero //= fator
+        else:
+            fator += 1
+    return numero
+def main():
+    numero = int(input("Insira um número inteiro positivo: "))
+    resultado = maior_fator_primo(numero)
+    print("O maior fator primo de", numero, "é:", resultado)
+main()
